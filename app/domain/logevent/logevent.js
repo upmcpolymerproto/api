9a4c541a6af94f1eb081deec4756fb3fabb64aee
upmcpolymerproto <santuccir@upmc.edu>
2017-03-07 13:57:55 -0500

    added private property to log model

--- app/domain/logevent/logevent.js
+++ app/domain/logevent/logevent.js
@@ -1,13 +1,19 @@
 'use strict';
 const BaseModel = require('../basemodel/basemodel');
 
-class LogEvent extends BaseModel {
+var _privateProp = "this is a private property";
+class LogEvent extends BaseModel {  
+
   constructor(type, user, value, fairWarning) {
     super("1", "logEvent");
     this.type = type;
     this.user = user;
     this.value = value;
     this.fairWarning = fairWarning || false;
+  }
+
+  get publicprop() {
+    return "concat public property with private: " + _privateProp;
   }
 
   get description() {
--- app/service/log/log.js
+++ app/service/log/log.js
@@ -4,9 +4,11 @@ const logEvent = require('../../domain/logevent/logevent');
 
 function LogService(app) {
     app.get('/log', (request, response) => {  
-        var event = new logEvent("Critical", "santuccir", "Test log message");
-        console.log(event.description);
+        var event = new logEvent("Critical", "santuccir", "Test log message");        
         console.log(event);
+        console.log(event.description);
+        console.log(event.publicprop);
+        console.log(event._privateProp);
         
         response.json(event);  
     })
