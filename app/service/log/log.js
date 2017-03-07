a03cce5a42c54a8ec4df686756f7399229f8f2ce
upmcpolymerproto <santuccir@upmc.edu>
2017-03-07 14:12:53 -0500

    added prop accessor to logevent.

--- app/service/log/log.js
+++ app/service/log/log.js
@@ -6,9 +6,10 @@ function LogService(app) {
     app.get('/log', (request, response) => {  
         var event = new logEvent("Critical", "santuccir", "Test log message");        
         console.log(event);
+        console.log(event.type);
         console.log(event.description);
         console.log(event.publicprop);
-        console.log(event._privateProp);
+        console.log(event._privateProp);        
         
         response.json(event);  
     })
