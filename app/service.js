b6b99c2bf543b0985f8e6594cbb4223cb67d9af8
Condeluci <condelucic@upmc.edu>
2017-03-09 11:05:48 -0500

    Added CORS fix for localhost:8080 for testing.

--- app/service.js
+++ app/service.js
@@ -9,6 +9,26 @@ var bodyParser = require('body-parser');
 //app.use(bodyParser.json()); // For parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // Create service modules
 const logService = require('../app/service/log/log');
 new logService(app);
